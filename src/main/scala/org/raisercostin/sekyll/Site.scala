0422e75d67fa44531514a506da694be777501c1f
raisercostin <raisercostin@gmail.com>
2017-08-18 20:48:37 +0300

    add flexmark as markdown renderer.

--- src/main/scala/org/raisercostin/sekyll/Site.scala
+++ src/main/scala/org/raisercostin/sekyll/Site.scala
@@ -7,6 +7,12 @@ import org.raisercostin.jedi.Locations
 import org.raisercostin.jedi.FileLocation
 import org.raisercostin.jedi.RelativeLocation
 import scala.util.Try
+import com.vladsch.flexmark.ext.abbreviation.AbbreviationExtension
+import com.vladsch.flexmark.ext.definition.DefinitionExtension
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension
+import com.vladsch.flexmark.ext.tables.TablesExtension
+import com.vladsch.flexmark.ext.typographic.TypographicExtension
+import java.util.Arrays
 
 case class Customer(image: String)
 case class Solution(name: String, link: String)
@@ -96,15 +102,50 @@ case class Site(currentLagomVersion: String, currentDocsVersion: String,
 
   val allCollections: Seq[SiteDocument] = RawSite.rawCollections(this).toSeq
 
+  import com.vladsch.flexmark.util.options.MutableDataSet
+  import com.vladsch.flexmark.parser.Parser
+  import com.vladsch.flexmark.html.HtmlRenderer
+  import com.vladsch.flexmark.ast.Node
+  
+  lazy val options = asKramdown()
+
+    // uncomment to set optional extensions
+    //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
+
+    // uncomment to convert soft-breaks to hard breaks
+    //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+    lazy val parser = Parser.builder(options).build();
+    lazy val renderer = HtmlRenderer.builder(options).build();
+  
   import play.doc.PrettifyVerbatimSerializer
   import org.pegdown.{ Extensions, LinkRenderer, PegDownProcessor, VerbatimSerializer }
   import scala.collection.JavaConverters._
   private lazy val pegdown = new PegDownProcessor(Extensions.ALL)
   def markdownToHtml(markdown: String) = {
-    pegdown.markdownToHtml(markdown, new LinkRenderer,
-      Map[String, VerbatimSerializer](VerbatimSerializer.DEFAULT -> PrettifyVerbatimSerializer).asJava)
+    val document:Node = parser.parse(markdown)
+    val html = renderer.render(document)
+    html
+//    pegdown.markdownToHtml(markdown, new LinkRenderer,
+//      Map[String, VerbatimSerializer](VerbatimSerializer.DEFAULT -> PrettifyVerbatimSerializer).asJava)
   }
+  
+  def asKramdown():MutableDataSet = {
+    import com.vladsch.flexmark.parser._;
+        val options = new MutableDataSet()
+        options.setFrom(ParserEmulationProfile.KRAMDOWN)
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+                AbbreviationExtension.create(),
+                DefinitionExtension.create(),
+                FootnoteExtension.create(),
+                TablesExtension.create(),
+                TypographicExtension.create()
+        ));
+        options
+    }
 }
+
+
 
 case class SiteConfig(yaml: Yaml) {
   val baseUrl = yaml.getString("baseUrl")
